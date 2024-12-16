e242c6a1faa5057d6eed4640c4986164bf7f5ac7
rahulprops <rahulhack912@gmail.com>
2024-12-16 23:51:20 +0530

    registor user complate

--- apis/controller/user/registor.js
+++ apis/controller/user/registor.js
@@ -1,30 +1,41 @@
 import { handleError } from "../../middleware/error/HandleError.js";
 import userModal from "../../modal/user.modal.js";
-
-export const registor= async (req,res)=>{
-    const {userName,userEmail,userPassword}=req.body;
-     if(userName && userEmail && userPassword){
-        try{
-            const isValidUser= await userModal.findOne({userEmail:userEmail}).select("-userPassword")
-            if(isValidUser){
-                return handleError(res,400,false,"user already exist")
-            }
-             const createUser= new userModal({
-                userName:userName,
-                userEmail:userEmail,
-                userPassword:userPassword
-             })
-             if(createUser){
-                const registor=await createUser.save()
-                 return handleError(res,201,true,"registor user sucessful",registor)
-             }else{
-                return handleError(res,400,false,"registor user failed")
-             }
-        }catch(err){
-            return handleError(res,500,false,`server error:${err}`)
-        }
-     }else{
-        return handleError(res,400,false,"all feils requied")
-     }
-    
-}
+import validator from "validator";
+import bcrypt from "bcrypt";
+//! user registor 
+export const registor = async (req, res) => {
+  const { userName, userEmail, userPassword } = req.body;
+  if (userName && userEmail && userPassword) {
+    // email check email valid or not
+    if (!validator.isEmail(userEmail)) {
+      return handleError(res, 400, false, "please enter valid email");
+    }
+    try {
+      // check user exist or not
+      const isValidUser = await userModal
+        .findOne({ userEmail: userEmail })
+        .select("-userPassword");
+      if (isValidUser) {
+        return handleError(res, 400, false, "user already exist");
+      }
+      // password hash
+      const hashPassword = await bcrypt.hash(userPassword, 12);
+      // create user
+      const createUser = new userModal({
+        userName: userName,
+        userEmail: userEmail,
+        userPassword: hashPassword,
+      });
+      if (createUser) {
+        const registor = await createUser.save();
+        return handleError(res, 201, true, "registor user sucessful", registor);
+      } else {
+        return handleError(res, 400, false, "registor user failed");
+      }
+    } catch (err) {
+      return handleError(res, 500, false, `server error:${err}`);
+    }
+  } else {
+    return handleError(res, 400, false, "all feils requied");
+  }
+};
