5a6628ba7da41089932d1356f1f362d2483a8b48
rahulprops <rahulhack912@gmail.com>
2024-12-17 14:08:51 +0530

    done findbyid product

--- apis/controller/product/product.controller.js
+++ apis/controller/product/product.controller.js
@@ -3,6 +3,7 @@ import path from 'path'
 import { handleError } from '../../middleware/error/HandleError.js'
 import { runInNewContext } from 'vm'
 import productModal from '../../modal/product.modal.js'
+import mongoose from 'mongoose'
 
 //! path 
 const Path= path.join("public/product")
@@ -16,8 +17,9 @@ const store= multer.diskStorage({
         return cb(null,file.originalname)
     }
 })
-export const productMulter= multer({storage:store})
 
+export const productMulter= multer({storage:store})
+//! add product
 export const productAdd= async (req,res)=>{
     const {productTitle,productDesc,productPrice,productCategory,productqty}=req.body;
     const image=req.file;
@@ -50,3 +52,37 @@ export const productAdd= async (req,res)=>{
         return handleError(res,500,false,`server error ${err}`)
     }
 }
+//! get all products
+export const allProduct= async (req,res)=>{
+    try{
+        const getProduct= await productModal.find()
+        if(getProduct){
+            return handleError(res,200,true ,"find product sucessful",getProduct)
+        }else{
+            return handleError(res,400,false,"find product failed")
+        }
+    }catch(err){
+        return handleError(res,500,false,`server error ${err}`)
+    }
+}
+//! find by id
+export const findByIdProduct= async (req,res)=>{
+    const {id}=req.params;
+    if(!id){
+        return handleError(res,400,false,"id not found")
+    }
+    if(!mongoose.Types.ObjectId.isValid(id)){
+        return handleError(res,400,"please enter valid id")
+    }
+    try{
+        const findById=await productModal.findById(id)
+        if(findById){
+            return handleError(res,200,true,"find product sucessful",findById)
+        }else{
+            return handleError(res,400,false,"find product failed")
+        }
+    }catch(err){
+        return handleError(res,500,false,`server error ${err}`)
+    }
+}
+
--- apis/roter/product/product.roter.js
+++ apis/roter/product/product.roter.js
@@ -1,6 +1,9 @@
 import express from 'express'
-import { productAdd, productMulter } from '../../controller/product/product.controller.js'
+import { allProduct, findByIdProduct, productAdd, productMulter } from '../../controller/product/product.controller.js'
 const productRouter=express.Router()
 
-productRouter.post("/add",productMulter.single("image"),productAdd) // => api/product/add
+productRouter.post("/add",productMulter.single("image"),productAdd) // => api/v1/product/add
+productRouter.get("/all",allProduct)                                // => api/v1/product/all
+productRouter.get("/:id",findByIdProduct)                           //=> api/v1/product/id
+
 export default productRouter;
