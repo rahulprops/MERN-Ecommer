ee1d59c215d20a9a87883c4b48eaccc16e653fc8
rahulprops <rahulhack912@gmail.com>
2024-12-17 09:32:47 +0530

    create product schema

---
+++ apis/modal/product.modal.js
@@ -0,0 +1,31 @@
+import mongoose, { Schema } from "mongoose";
+
+const productSchema=new Schema({
+    productTitel:{
+        type:String,
+        required:[true,"title is required"]
+    },
+    productDescription:{
+        type:String,
+        required:[true,"description is required"]
+    },
+    productImage:{
+        type:String,
+        required:[true,"image is requied"]
+    },
+    productPrice:{
+        type:Number,
+        required:[true,"price is requied"]
+    },
+    productCategory:{
+        type:String,
+        required:[true,"categroy is required"]
+    },
+    prodductQty:{
+        type:String,
+        required:[true,"qty is requied"]
+    }
+
+},{timestamps:true})
+const productModal= mongoose.model("product",productSchema)
+export default productModal
